a516f430c67c0e01a7e158b4273874e359caa49c
mtmjmc <juanmcruz997@gmail.com>
2023-11-15 14:49:41 -0300

    coreccion index

--- js/ejercicio-1.js
+++ js/ejercicio-1.js
@@ -1,7 +1,20 @@
-// Crear el array meses
-var meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
 
-// Mostrar los meses en forma de lista
+var meses = ["enero",
+ "febrero", 
+ "marzo", 
+ "abril", 
+ "mayo", 
+ "junio", 
+ "julio", 
+ "agosto", 
+ "septiembre", 
+ "octubre", 
+ "noviembre", 
+ "diciembre"];
+
+
 console.log("Lista de meses:");
+
 for (var i = 0; i < meses.length; i++) {
+    
     console.log(meses[i]);}

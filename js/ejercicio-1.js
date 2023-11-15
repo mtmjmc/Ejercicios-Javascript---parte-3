ad71232a5aa5e1267323066d40b0f865fc03f52c
mtmjmc <juanmcruz997@gmail.com>
2023-11-15 14:45:07 -0300

    subir trabajo
    1

---
+++ js/ejercicio-1.js
@@ -0,0 +1,7 @@
+// Crear el array meses
+var meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+
+// Mostrar los meses en forma de lista
+console.log("Lista de meses:");
+for (var i = 0; i < meses.length; i++) {
+    console.log(meses[i]);}
